1fde16f76a1bf8cc95f48d7162b7d4eca3393c30
Etornam <sunumacbright@gmail.com>
2021-11-13 20:08:58 +0000

    json to object model class

---
+++ lib/core/data/coin.dart
@@ -0,0 +1,55 @@
+class Coin {
+    Coin({
+        this.id,
+        this.rank,
+        this.symbol,
+        this.name,
+        this.supply,
+        this.maxSupply,
+        this.marketCapUsd,
+        this.volumeUsd24Hr,
+        this.priceUsd,
+        this.changePercent24Hr,
+        this.vwap24Hr,
+    });
+
+    String id;
+    String rank;
+    String symbol;
+    String name;
+    String supply;
+    String maxSupply;
+    String marketCapUsd;
+    String volumeUsd24Hr;
+    String priceUsd;
+    String changePercent24Hr;
+    String vwap24Hr;
+
+    factory Coin.fromJson(Map<String, dynamic> json) => Coin(
+        id: json["id"],
+        rank: json["rank"],
+        symbol: json["symbol"],
+        name: json["name"],
+        supply: json["supply"],
+        maxSupply: json["maxSupply"],
+        marketCapUsd: json["marketCapUsd"],
+        volumeUsd24Hr: json["volumeUsd24Hr"],
+        priceUsd: json["priceUsd"],
+        changePercent24Hr: json["changePercent24Hr"],
+        vwap24Hr: json["vwap24Hr"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "id": id,
+        "rank": rank,
+        "symbol": symbol,
+        "name": name,
+        "supply": supply,
+        "maxSupply": maxSupply,
+        "marketCapUsd": marketCapUsd,
+        "volumeUsd24Hr": volumeUsd24Hr,
+        "priceUsd": priceUsd,
+        "changePercent24Hr": changePercent24Hr,
+        "vwap24Hr": vwap24Hr,
+    };
+}
--- lib/core/data/coinbase_asset.dart
+++ lib/core/data/coinbase_asset.dart
@@ -1,5 +1,34 @@
-/// TODO: Complete the [CoinCapAsset] class
+/// TODO: Complete the [CoinCapAsset] class - DONE
 /// and also implement methods for converting to and from JSON.
 ///
 /// You are free to use any helper package
-class CoinCapAsset {}
+// To parse this JSON data, do
+//
+//     final coinCapAsset = coinCapAssetFromJson(jsonString);
+
+import 'dart:convert';
+
+
+CoinCapAsset coinCapAssetFromJson(String str) => CoinCapAsset.fromJson(json.decode(str));
+
+String coinCapAssetToJson(CoinCapAsset data) => json.encode(data.toJson());
+
+class CoinCapAsset {
+    CoinCapAsset({
+        this.data,
+        this.timestamp,
+    });
+
+    List<Map<String, String>> data;
+    int timestamp;
+
+    factory CoinCapAsset.fromJson(Map<String, dynamic> json) => CoinCapAsset(
+        data: List<Map<String, String>>.from(json["data"].map((x) => Map.from(x).map((k, v) => MapEntry<String, String>(k, v == null ? null : v)))),
+        timestamp: json["timestamp"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "data": List<dynamic>.from(data.map((x) => Map.from(x).map((k, v) => MapEntry<String, dynamic>(k, v == null ? null : v)))),
+        "timestamp": timestamp,
+    };
+}
