9b662278bb2624953470a4bfed72a78523e11948
Etornam <sunumacbright@gmail.com>
2021-11-17 18:23:51 +0000

    paginate data based on offset and fix error with search

--- lib/pages/assets_page.dart
+++ lib/pages/assets_page.dart
@@ -17,27 +17,26 @@ class _AssetsPageState extends State<AssetsPage> {
   final ScrollController _scrollController = ScrollController();
   Timer _searchDebounce;
   AssetsProvider _assetsProvider;
-  int limit = 30;
+  int offset = 0;
+  int totalLength = 0;
 
   void onSearchQueryChanged(String search) {
     if (search != null) {
-      if (_searchDebounce != null) {
-        _searchDebounce.cancel();
-        setState(() {
-          _searchDebounce = Timer(Duration(seconds: 2), () {
-            if (search.length > 2) {
-              _assetsProvider.fetchAssets(search: search);
-            }
-          });
+      _searchDebounce?.cancel();
+      setState(() {
+        _searchDebounce = Timer(Duration(seconds: 1), () {
+          if (search.length > 2) {
+            _assetsProvider.fetchAssets(search: search, offset: 0);
+          }
         });
-      }
+      });
     } else {
-      _assetsProvider.fetchAssets();
+      _assetsProvider.fetchAssets(offset: 0);
     }
   }
 
   void didBuild(BuildContext context) {
-    _assetsProvider.fetchAssets();
+    _assetsProvider.fetchAssets(offset: 0);
   }
 
   @override
@@ -59,11 +58,9 @@ class _AssetsPageState extends State<AssetsPage> {
   _getMoreData() {
     print('at bottom');
     setState(() {
-      limit = limit + limit;
+      offset = totalLength;
     });
-    // present = present + 1;
-    // perPage = perPage + perPage;
-    _assetsProvider.fetchAssets(limit: limit);
+    _assetsProvider.fetchAssets(offset: offset);
   }
 
   @override
@@ -75,7 +72,8 @@ class _AssetsPageState extends State<AssetsPage> {
     return StreamBuilder<AssetsProviderEvent>(
       stream: _assetsProvider.stream,
       builder: (context, snapshot) {
-        final _isLoading = snapshot.data?.state == ProviderState.LOADING;
+        final _isLoading = snapshot.data?.state == ProviderState.LOADING &&
+            snapshot.data?.data == null;
 
         return Scaffold(
           appBar: AppBar(
@@ -134,7 +132,8 @@ class _AssetsPageState extends State<AssetsPage> {
           body: SafeArea(
             child: RefreshIndicator(
               onRefresh: () {
-                return _assetsProvider.fetchAssets(refresh: true);
+                return _assetsProvider.fetchAssets(
+                    refresh: true, offset: offset);
               },
               child: Builder(
                 builder: (context) {
@@ -146,51 +145,73 @@ class _AssetsPageState extends State<AssetsPage> {
 
                   if (snapshot.data?.state == ProviderState.ERROR &&
                       snapshot.data?.data != null) {
-                    return Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.error,
-                          color: Colors.red,
-                        ),
-                        Text('${snapshot.data.data}'),
-                      ],
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 40, vertical: 10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.error,
+                            color: Colors.red,
+                            size: 40,
+                          ),
+                          Text(
+                            '${snapshot.data.data.message}',
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
                     );
                   }
 
                   if (!_isLoading &&
                       snapshot.data?.state == ProviderState.SUCCESS &&
                       snapshot.data?.data != null) {
+                    totalLength = totalLength + snapshot.data.data.length;
                     return ListView.separated(
                         shrinkWrap: true,
                         controller: _scrollController,
                         itemBuilder: (context, index) {
-                          if (index == snapshot.data.data.length) {
-                            return Center(
-                                child: CircularProgressIndicator.adaptive());
-                          }
-                          Coin coin = Coin.fromJson(snapshot.data.data[index]);
+                          Coin coin = snapshot.data.data[index];
                           return ListTile(
-                            leading: Text('${index + 1}'),
+                            leading: Text('${coin.rank}'),
                             title: Text('${coin.name}'),
                             subtitle: Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
                                 Text('Market Cap.:'),
-                                Text('${coin.marketCapUsd}'),
+                                Text('${coin.marketCapUsd ?? 'Not available'}'),
                               ],
                             ),
                             trailing: Text(
-                                '${NumberFormat.currency(name: 'US\$', decimalDigits: 2).format(double.parse(coin.priceUsd))}'),
+                                '${NumberFormat.currency(name: 'US\$', decimalDigits: 2).format(double.parse(coin.priceUsd ?? '0'))}'),
                           );
                         },
                         separatorBuilder: (__, _) => Divider(),
-                        itemCount: snapshot.data.data.length);
+                        itemCount: snapshot.data == null
+                            ? 0
+                            : snapshot.data.data.length);
                   }
-
-                  return Center(
-                    child: Text('Fetch & Show Assets'),
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 40, vertical: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.sentiment_dissatisfied,
+                          color: Colors.red,
+                          size: 40,
+                        ),
+                        Text(
+                          'There\'s no data available at the moment',
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
                   );
                 },
               ),
