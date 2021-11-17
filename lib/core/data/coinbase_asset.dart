57de405563fef89453cd76db624ad792ba699007
Etornam <sunumacbright@gmail.com>
2021-11-17 18:08:39 +0000

    generate model class from json response

--- lib/core/data/coinbase_asset.dart
+++ lib/core/data/coinbase_asset.dart
@@ -2,33 +2,34 @@
 /// and also implement methods for converting to and from JSON.
 ///
 /// You are free to use any helper package
-// To parse this JSON data, do
-//
-//     final coinCapAsset = coinCapAssetFromJson(jsonString);
 
-import 'dart:convert';
+import 'coin.dart';
 
-
-CoinCapAsset coinCapAssetFromJson(String str) => CoinCapAsset.fromJson(json.decode(str));
-
-String coinCapAssetToJson(CoinCapAsset data) => json.encode(data.toJson());
 
 class CoinCapAsset {
-    CoinCapAsset({
-        this.data,
-        this.timestamp,
-    });
+  List<Coin> data;
+  int timestamp;
 
-    List<Map<String, String>> data;
-    int timestamp;
+  CoinCapAsset({this.data, this.timestamp});
 
-    factory CoinCapAsset.fromJson(Map<String, dynamic> json) => CoinCapAsset(
-        data: List<Map<String, String>>.from(json["data"].map((x) => Map.from(x).map((k, v) => MapEntry<String, String>(k, v == null ? null : v)))),
-        timestamp: json["timestamp"],
-    );
+  CoinCapAsset.fromJson(Map<String, dynamic> json) {
+    if (json['data'] != null) {
+      data = <Coin>[];
+      json['data'].forEach((v) {
+        data.add(new Coin.fromJson(v));
+      });
+    }
+    timestamp = json['timestamp'];
+  }
 
-    Map<String, dynamic> toJson() => {
-        "data": List<dynamic>.from(data.map((x) => Map.from(x).map((k, v) => MapEntry<String, dynamic>(k, v == null ? null : v)))),
-        "timestamp": timestamp,
-    };
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.data != null) {
+      data['data'] = this.data.map((v) => v.toJson()).toList();
+    }
+    data['timestamp'] = this.timestamp;
+    return data;
+  }
+
+  
 }
